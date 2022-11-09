d83255960b02fd0d3c3ffff0407b9808e367ccb2
BlaiseComo <comoblaise@gmail.com>
2022-11-08 19:30:46 -0700

    Added please_work_pandas

---
+++ please_work_pandas/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib"
+XML2_LIBS="-lxml2 -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -lz -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -llzma -lpthread -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -liconv -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/include/libxml2 -I/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ please_work_pandas/env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib"
+XSLT_LIBS="-lxslt -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -lxml2 -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -lpthread -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -liconv -L/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/blaisecomo/Documents/jupyterNotebookReference/please_work_pandas/env/include"
+MODULE_VERSION="xslt-1.1.35"
