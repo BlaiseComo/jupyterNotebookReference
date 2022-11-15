b15c21c86291789691e82719d70470f5bba49f6b
BlaiseComo <comoblaise@gmail.com>
2022-11-15 16:28:08 -0700

    Added first project

---
+++ personal_project_1/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib"
+XML2_LIBS="-lxml2 -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -lz -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -llzma -lpthread -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -liconv -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/include/libxml2 -I/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ personal_project_1/env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib"
+XSLT_LIBS="-lxslt -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -lxml2 -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -lpthread -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -liconv -L/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/blaisecomo/Documents/jupyterNotebookReference/personal_project_1/env/include"
+MODULE_VERSION="xslt-1.1.35"
