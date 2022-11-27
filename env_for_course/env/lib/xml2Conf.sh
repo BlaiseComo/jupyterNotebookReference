0bc181c93203dd2fff4efd3aed78097316e21992
BlaiseComo <comoblaise@gmail.com>
2022-11-27 07:44:59 -0700

    Added env for course

---
+++ env_for_course/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/blaisecomo/Documents/jupyterNotebookReference/env_for_course/env/lib"
+XML2_LIBS="-lxml2 -L/Users/blaisecomo/Documents/jupyterNotebookReference/env_for_course/env/lib -lz -L/Users/blaisecomo/Documents/jupyterNotebookReference/env_for_course/env/lib -llzma -lpthread -L/Users/blaisecomo/Documents/jupyterNotebookReference/env_for_course/env/lib -liconv -L/Users/blaisecomo/Documents/jupyterNotebookReference/env_for_course/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/blaisecomo/Documents/jupyterNotebookReference/env_for_course/env/include/libxml2 -I/Users/blaisecomo/Documents/jupyterNotebookReference/env_for_course/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
